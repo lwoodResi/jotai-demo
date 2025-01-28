252ac45f92d687b9bba5f3ec7ef4206f81d63db0
lwoodResi <logan.wood@resi.io>
2025-01-28 15:05:26 -0600

    exploring

--- src/SafeJotaiComponents/useCount.tsx
+++ src/SafeJotaiComponents/useCount.tsx
@@ -4,25 +4,34 @@ import {routeAtom} from '../globalStore';
 // NOTE: I'm calling this safe because we are not exposing the atoms directly to the components.
 // Instead, we can compose them further and export through a hook, giving access only to the
 // getters and setters we want.
-const safeCountAtom = atom(0);
-const safeCountBAtom = atom(0);
-
-// This is a derived readonly atom. We can access different atoms and compose the values
-// and return them as a part of this atom.
-const safeReadOnlyCountAtoms = atom(get => {
+export const safeCountAtom = atom(0);
+export const safeCountBAtom = atom(0);
+export const safeReadOnlyCountAtoms = atom(get => {
   const countA = get(safeCountAtom);
   const countB = get(safeCountBAtom);
-  const safeCountTotal = countA + countB; // deriving value from other atoms
-  const isOnSafePageA = get(routeAtom) === 'safePageA'; // getting value from global store when composing atom
-  return {countA, countB, isOnSafePageA, safeCountTotal};
+  const safeCountTotal = countA + countB;
+  return safeCountTotal;
 });
+// This is a derived readonly atom. We can access different atoms and compose the values
+// and return them as a part of this atom.
+// export const safeReadOnlyCountAtoms = atom(get => {
+//   const countA = get(safeCountAtom);
+//   const countB = get(safeCountBAtom);
+//   const safeCountTotal = countA + countB; // deriving value from other atoms
+//   const isOnSafePageA = get(routeAtom) === 'safePageA'; // getting value from global store when composing atom
+//   return {countA, countB, isOnSafePageA, safeCountTotal};
+// });
 
 export const useCount = () => {
   const setSafeCountA = useSetAtom(safeCountAtom);
   const setSafeCountB = useSetAtom(safeCountBAtom);
-  const {countA, countB, isOnSafePageA, safeCountTotal} = useAtomValue(
-    safeReadOnlyCountAtoms
-  );
+  const countA = useAtomValue(safeCountAtom);
+  const countB = useAtomValue(safeCountBAtom);
+  const safeCountTotal = useAtomValue(safeReadOnlyCountAtoms);
+  const isOnSafePageA = useAtomValue(routeAtom) === 'safePageA';
+  // const {countA, countB, isOnSafePageA, safeCountTotal} = useAtomValue(
+  //   safeReadOnlyCountAtoms
+  // );
 
   return {
     countA,

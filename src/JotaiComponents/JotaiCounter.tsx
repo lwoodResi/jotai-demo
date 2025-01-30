727e97ad7a3702c95743591e541c13a1cce08fa8
lwoodResi <logan.wood@resi.io>
2025-01-30 09:50:43 -0600

    note

--- src/JotaiComponents/JotaiCounter.tsx
+++ src/JotaiComponents/JotaiCounter.tsx
@@ -1,25 +1,18 @@
 import {atom, useAtom} from 'jotai';
-import * as React from 'react';
 
+// NOTE: creating an atom outside of the component avoids
+// infinite re-renders because atom() would be called every time
 const countAAtom = atom(0);
 const countBAtom = atom(0);
+
 const JotaiCounter = () => {
   const [countA, setCountA] = useAtom(countAAtom);
   const [countB, setCountB] = useAtom(countBAtom);
 
-  React.useEffect(() => {
-    console.count(`jotai countA: ${countA}`);
-  }, [countA]);
-
-  React.useEffect(() => {
-    console.count(`jotai countB: ${countB}`);
-  }, [countB]);
   return (
     <div>
       <h1>Jotai Counter</h1>
-      <br />
       <button onClick={() => setCountA(c => c + 1)}>Count A: {countA}</button>
-      <h1 />
       <button onClick={() => setCountB(c => c + 1)}>Count B: {countB}</button>
     </div>
   );
--- src/ReactComponents/ReactCounter.tsx
+++ src/ReactComponents/ReactCounter.tsx
@@ -4,19 +4,10 @@ const ReactCounter = () => {
   const [countA, setCountA] = React.useState(0);
   const [countB, setCountB] = React.useState(0);
 
-  React.useEffect(() => {
-    console.count(`react countA: ${countA}`);
-  }, [countA]);
-
-  React.useEffect(() => {
-    console.count(`react countB: ${countB}`);
-  }, [countB]);
   return (
     <div>
       <h1>React Counter</h1>
-      <br />
       <button onClick={() => setCountA(c => c + 1)}>Count A: {countA}</button>
-      <h1 />
       <button onClick={() => setCountB(c => c + 1)}>Count B: {countB}</button>
     </div>
   );
